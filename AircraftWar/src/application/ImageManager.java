29d3039d2fc83ccd570d522ff0de465dd60953bc
hilaolu <i@phyzait.moe>
2022-03-15 02:34:10 -0400

    refactor: images -> resources

--- AircraftWar/src/application/ImageManager.java
+++ AircraftWar/src/application/ImageManager.java
@@ -27,23 +27,39 @@ public class ImageManager {
 
 	public static BufferedImage BACKGROUND_IMAGE;
 	public static BufferedImage HERO_IMAGE;
+
 	public static BufferedImage HERO_BULLET_IMAGE;
 	public static BufferedImage ENEMY_BULLET_IMAGE;
+
 	public static BufferedImage MOB_ENEMY_IMAGE;
+	public static BufferedImage ELITE_ENEMY_IMAGE;
+	public static BufferedImage BOSS_ENEMY_IMAGE;
+
+	public static BufferedImage BLOOD_ITEM_IMAGE;
+	public static BufferedImage BOMB_ITEM_IMAGE;
+	public static BufferedImage BULLET_ITEM_IMAGE;
 
 	static {
 		try {
 
-			BACKGROUND_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/src/images/bg.jpg"));
+			BACKGROUND_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/bg.jpg"));
 
-			HERO_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/src/images/hero.png"));
-			MOB_ENEMY_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/src/images/mob.png"));
-			HERO_BULLET_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/src/images/bullet_hero.png"));
-			ENEMY_BULLET_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/src/images/bullet_enemy.png"));
+			HERO_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/hero.png"));
+			HERO_BULLET_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/bullet_hero.png"));
+			ENEMY_BULLET_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/bullet_enemy.png"));
+
+			MOB_ENEMY_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/mob.png"));
+			ELITE_ENEMY_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/elite.png"));
+			BOSS_ENEMY_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/boss.png"));
+
+			BLOOD_ITEM_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/prop_blood.png"));
+			BOMB_ITEM_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/prop_bomb.png"));
+			BULLET_ITEM_IMAGE = ImageIO.read(new FileInputStream("AircraftWar/resources/images/prop_bullet.png"));
 
 			CLASSNAME_IMAGE_MAP.put(HeroAircraft.class.getName(), HERO_IMAGE);
 			CLASSNAME_IMAGE_MAP.put(EnemyAircraft.class.getName(), MOB_ENEMY_IMAGE);
 			CLASSNAME_IMAGE_MAP.put(HeroBullet.class.getName(), HERO_BULLET_IMAGE);
+
 			CLASSNAME_IMAGE_MAP.put(EnemyBullet.class.getName(), ENEMY_BULLET_IMAGE);
 
 		} catch (IOException e) {
