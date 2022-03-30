07a37760410f9c61a1fc43aa869033e032de9894
hilaolu <i@phyzait.moe>
2022-03-30 01:30:30 -0400

    test: add ItemEffect spec

--- AircraftWar/test/src/ItemSpec.scala
+++ AircraftWar/test/src/ItemSpec.scala
@@ -10,6 +10,7 @@ import factory.BulletFactory
 
 import misc.typing.EnemyType._
 import misc.typing._
+import application.Game
 
 class ItemCollisionSpec extends AnyFlatSpec with should.Matchers {
 
@@ -33,5 +34,21 @@ class ItemCollisionSpec extends AnyFlatSpec with should.Matchers {
               hero.getLocationY() + 100
             )
         item.crash(hero) should be(false)
+    }
+}
+
+class ItemEffectSpec extends AnyFlatSpec with should.Matchers {
+    "Blood" should "increase hp then vanish" in {
+        val game = Game
+        val hero = game.getHero()
+        hero.decreaseHp(114514)
+        val blood = ItemFactory.spawn(
+          ItemType.BLOOD,
+          hero.getLocationX(),
+          hero.getLocationY()
+        )
+        blood.effect(game)
+        blood.isValid() should be(false)
+        hero.getHp() should be(30) // may change?
     }
 }
