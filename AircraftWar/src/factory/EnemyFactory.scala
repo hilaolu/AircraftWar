eebc4dafc6d58d55948789b1fa132a6d068c08b6
hilaolu <i@phyzait.moe>
2022-03-29 23:52:30 -0400

    feat: add enemyfactory.spawn(type,loc) method

--- AircraftWar/src/factory/EnemyFactory.scala
+++ AircraftWar/src/factory/EnemyFactory.scala
@@ -5,45 +5,44 @@ import application.Main
 import application.ImageManager
 
 object EnemyFactory {
-    def spawn(t: String): AbstractAircraft = {
+    def spawn(t: String, location: (Int, Int)): AbstractAircraft = {
         if (t == "e") {
             new EliteEnemy(
-              (
-                Math.random() * (Main.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE
-                    .getWidth())
-              ).toInt * 1,
-              (Math
-                  .random() * Main.WINDOW_HEIGHT * 0.2).toInt * 1,
+              location._1,
+              location._2,
               0,
               3,
               20
             )
         } else if (t == "t") {
             new TrivialEnemy(
-              (
-                Math.random() * (Main.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE
-                    .getWidth())
-              ).toInt * 1,
-              (Math
-                  .random() * Main.WINDOW_HEIGHT * 0.2).toInt * 1,
+              location._1,
+              location._2,
               0,
               5,
               10
             )
         } else if (t == "b") {
             new BossEnemy(
-              (
-                Math.random() * (Main.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE
-                    .getWidth())
-              ).toInt * 1,
-              (Math
-                  .random() * Main.WINDOW_HEIGHT * 0.2).toInt * 1,
+              location._1,
+              location._2,
               0,
               5,
               10
             )
         } else {
-            this.spawn("t")
+            this.spawn("t", location)
         }
+    }
+
+    def spawn(t: String): AbstractAircraft = {
+        val random_location = (
+          (
+            Math.random() * (Main.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE
+                .getWidth())
+          ).toInt * 1,
+          (Math.random() * Main.WINDOW_HEIGHT * 0.2).toInt * 1
+        )
+        this.spawn(t, random_location)
     }
 }
